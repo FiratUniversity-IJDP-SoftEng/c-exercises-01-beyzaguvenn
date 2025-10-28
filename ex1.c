974c8b6c5fc9b4f257d5fd8a0770352e91ea1ba6
beyzaguvenn <beyzaguven281@gmail.com>
2025-10-28 15:38:58 +0300

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    const int SECRET_NUMBER = 3;
+    int guess;
+
+    printf("1 ile 5 arasında bir sayi tahmin ediniz: ");
+
+    scanf("%d", &guess);
+
+    if (guess == SECRET_NUMBER) {
+        printf("\nDoğru sayı!\n", SECRET_NUMBER);
+    } else {
+        printf("\nYanlış tahmin!\n", SECRET_NUMBER);
+    }
+
+    return 0;
+}
