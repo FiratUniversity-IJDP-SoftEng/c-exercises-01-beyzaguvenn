87c83a7d818619e9e6b9e8ef4fd2765e7db59392
beyzaguvenn <beyzaguven281@gmail.com>
2025-10-28 15:46:42 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+int main() {
+    int age;
+    int i;  
 
+printf("Yaşınızı giriniz: ");
+
+scanf("%d", &age);
+
+    for (i = 0; i < age; i++) {
+        printf("Happy Birthday!\n");
+    }
+    return 0;
+}
