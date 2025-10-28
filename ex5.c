0f89290db73283afdd17cc0586ddbe37ab8f19ea
beyzaguvenn <beyzaguven281@gmail.com>
2025-10-28 16:13:58 +0300

    Update ex5.c

--- ex5.c
+++ ex5.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+void sayHello(const char *name);
+
+int main() {
+    char name[50]; 
+
+    printf("Enter your name: ");
+scanf("%s", name);
+
+    sayHello(name);
+
+    return 0;
+}
+
+void sayHello(const char *name) {
+    printf("Hello, %s\n", name);
+}
