1809f7c3fe03690b48ce223eda9fa7387ed49af3
beyzaguvenn <beyzaguven281@gmail.com>
2025-10-28 15:57:33 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -1,2 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
 
+    char pet_type[3];
+
+    printf("Do you have a cat or a dog?");
+
+    scanf("%s", pet_type);
+
+    // Yanıtın "kedi" olup olmadığını kontrol ediyoruz.
+    // strcmp, iki dize (string) eşitse 0 döndürür.
+    if (strcmp(pet_type, "cat") == 0) {
+        printf("meow\n");
+    } 
+    else if (strcmp(pet_type, "dog") == 0) {
+        printf("hav hav\n");
+    } 
+    else {
+        printf("Type cat or dog\n");
+    }
+    return 0; // Başarılı program sonu
+}
 
